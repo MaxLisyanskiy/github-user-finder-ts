3d885a459a63f2f9ef30fe903de1c6ba97c9e9e3
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-11-01 15:05:29 +0300

    feat: added user card

---
+++ src/components/UserCard/UserCard.tsx
@@ -0,0 +1,14 @@
+import { UserStat, UserStatProps } from "components/UserStat";
+import styles from "./UserCard.module.scss";
+
+interface UserCardProps extends UserStatProps {}
+
+export const UserCard = (props: UserCardProps) => (
+  <div className={styles.userCard}>
+    <UserStat
+      repos={props.repos}
+      followers={props.followers}
+      following={props.following}
+    />
+  </div>
+);
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "MaxLisyanskiy",
+  avatar: "https://avatars.githubusercontent.com/u/59924323?v=4",
+  name: "Maksim Lisianskiy",
+  company: "Magnit",
+  blog: "https://pikabu.ru/@MaxLisyanskiy",
+  location: "Saint-Petersburg, Russia",
+  bio: "Frontend developer",
+  twitter: "",
+  repos: 54,
+  followers: 0,
+  following: 1,
+  created: "2020-01-15T14:32:32Z",
+};
