0fe4a6b4eb0ced835557888705f16e9a8faa4c3b
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-11-03 16:11:23 +0300

    feat: added user-info

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,14 +1,34 @@
-import { UserStat, UserStatProps } from "components/UserStat";
+import { UserInfo } from "components/UserInfo";
+import { UserStat } from "components/UserStat";
+import { UserTitle } from "components/UserTitle";
+import { LocalGithubUser } from "types";
 import styles from "./UserCard.module.scss";
 
-interface UserCardProps extends UserStatProps {}
+interface UserCardProps extends LocalGithubUser {}
 
-export const UserCard = (props: UserCardProps) => (
-  <div className={styles.userCard}>
-    <UserStat
-      repos={props.repos}
-      followers={props.followers}
-      following={props.following}
-    />
-  </div>
-);
+export const UserCard = (props: UserCardProps) => {
+  return (
+    <div className={styles.userCard}>
+      <img src={props.avatar} alt={props.login} className={styles.avatar} />
+      <UserTitle
+        name={props.name}
+        login={props.login}
+        created={props.created}
+      />
+      <p className={`${styles.bio}${props.bio ? "" : ` ${styles.empty}`}`}>
+        {props.bio || "This profile hasn't bio"}
+      </p>
+      <UserStat
+        repos={props.repos}
+        followers={props.followers}
+        following={props.following}
+      />
+      <UserInfo
+        blog={props.blog}
+        company={props.company}
+        location={props.location}
+        twitter={props.twitter}
+      />
+    </div>
+  );
+};
